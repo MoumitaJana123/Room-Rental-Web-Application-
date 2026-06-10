22ec50c83355d2ac96bd34c32dd9ac12ae45cb01
MoumitaJana123 <moumita2003jana@gmail.com>
2026-06-10 23:58:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
-const User = require("./models/user.js");
+//const User = require("./models/user.js");
 
 const listingRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
